a8ee2347eec045c537f2ed20a04b5c68d5f020ec
Viktor Justo <viktor@vjustov.me>
2014-07-13 01:43:16 -0400

    Adding Note test file.

---
+++ test/note_test.rb
@@ -0,0 +1,11 @@
+describe(Clef::Models::Note) do
+  describe("#interpret") do
+    it("by default, should return a center scale note")
+    it("by default, should return a single quater note")
+
+    it("should output valid ABC")
+
+    it("should error when input is invalid")
+
+  end
+end
