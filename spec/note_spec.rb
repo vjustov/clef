bd19407c78b27ba464acb9c012fdbb0cb89be2ef
Viktor Justo <viktor@vjusto.com>
2014-08-04 23:40:37 -0400

    Adding spec/ folder and some tests.

---
+++ spec/note_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper.rb'
+
+describe(Clef::Models::Note) do
+  describe("when creating a note") do
+    context("with valid parameters") do
+      before :all do
+        @note = Clef::Models::Note.new "C"
+      end
+
+      it("should create a Note object") do
+        @note.should be_a_kind_of(Clef::Models::Note)
+      end
+
+      it("should have a valid note") do
+        @note.pitch.should eq("C")
+      end
+      it("should have a valid length") do
+        @note.length.should eq("1/4")
+      end
+
+      it("should have an octave") do
+        @note.octave.should eq(8)
+      end
+    end
+  end
+  describe(".parse") do
+    it("by default, should return a center scale note")
+    it("by default, should return a single quater note")
+    it("should return a Note") do
+      Clef::Models::Note.parse('A3').should be_kind_of(Clef::Models::Note)
+    end
+    it("should parse valid ABC") do
+
+      Clef::Models::Note.parse('A3').should be_kind_of(Clef::Models::Note)
+      Clef::Models::Note.parse('A/2').should be_kind_of(Clef::Models::Note)
+      Clef::Models::Note.parse('a3').should be_kind_of(Clef::Models::Note)
+    end
+
+    it("should error when input is invalid")
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+["models"].each do |f|
+  require File.join(File.dirname(__FILE__), '..', "lib/#{f}.rb")
+end
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
